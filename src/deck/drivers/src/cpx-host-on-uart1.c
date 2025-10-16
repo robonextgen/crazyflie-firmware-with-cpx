2d43549abc2af3de6d24df1379985b7dec8f1446
Daniele Bortoluzzi <daniele.bortoluzzi@unito.it>
2025-10-16 14:36:33 +0200

    Add CPX UART1 configuration and implementation
    - Added cf2_cpx_uart1_defconfig with CPX configuration for UART1
    - Added cf2_cpx_uart1.config configuration file
    - Updated cpx-host-on-uart1.c driver implementation
    - Configured for 115200 baud rate on UART1
    - Enabled CPX host communication on UART1

--- src/deck/drivers/src/cpx-host-on-uart1.c
+++ src/deck/drivers/src/cpx-host-on-uart1.c
@@ -39,8 +39,53 @@
 #include "cpx_external_router.h"
 #include "cpx_uart1_transport.h"
 #include "cpx.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
 
 static bool isInit = false;
+#ifdef CONFIG_CPX_UART1_PING_TEST
+static TimerHandle_t pingTimer;
+#endif
+
+#ifdef CONFIG_CPX_UART1_PING_TEST
+static void pingTimerCallback(TimerHandle_t xTimer)
+{
+  static uint32_t pingCounter = 0;
+  CPXRoutablePacket_t pingPacket;
+  
+  DEBUG_PRINT("CPX Over UART1: Timer callback triggered - ping #%lu\n", (unsigned long)pingCounter);
+  
+  // Create a simple ping message
+  pingPacket.route.destination = CPX_T_WIFI_HOST; // Send to external host
+  pingPacket.route.source = CPX_T_STM32;
+  pingPacket.route.function = CPX_F_TEST; // Use TEST function
+  pingPacket.route.lastPacket = true;
+  pingPacket.route.version = CPX_VERSION;
+  pingPacket.dataLength = 4;
+  
+  // Ping data: counter
+  pingPacket.data[0] = (uint8_t)((pingCounter >> 24) & 0xFF);
+  pingPacket.data[1] = (uint8_t)((pingCounter >> 16) & 0xFF);
+  pingPacket.data[2] = (uint8_t)((pingCounter >> 8) & 0xFF);
+  pingPacket.data[3] = (uint8_t)(pingCounter & 0xFF);
+  
+  DEBUG_PRINT("CPX Over UART1: Sending ping #%lu\n", (unsigned long)pingCounter);
+  
+  // Send ping via CPX UART1 transport
+  cpxUART1TransportSend(&pingPacket);
+  
+  DEBUG_PRINT("CPX Over UART1: Ping #%lu sent successfully\n", (unsigned long)pingCounter);
+  
+  pingCounter++;
+  
+  // Stop after 10 pings to avoid flooding
+  if (pingCounter >= 10) {
+    DEBUG_PRINT("CPX Over UART1: Stopping ping timer after 10 pings\n");
+    xTimerStop(pingTimer, 0);
+  }
+}
+#endif
 
 static void cpxOverUart1Init(DeckInfo *info)
 {
@@ -60,7 +105,24 @@ static void cpxOverUart1Init(DeckInfo *info)
   
   DEBUG_PRINT("CPX Over UART1: Initializing CPX...\n");
   cpxInit();
-
+  
+#ifdef CONFIG_CPX_UART1_PING_TEST
+  // Create ping timer (send ping every 2 seconds)
+  DEBUG_PRINT("CPX Over UART1: Creating ping timer...\n");
+  pingTimer = xTimerCreate("CPXPing", 
+                          pdMS_TO_TICKS(2000), // 2 seconds
+                          pdTRUE,              // Auto-reload
+                          NULL,                // Timer ID
+                          pingTimerCallback);
+  
+  if (pingTimer != NULL) {
+    xTimerStart(pingTimer, 0);
+    DEBUG_PRINT("CPX Over UART1: Ping timer started (every 2 seconds)\n");
+  } else {
+    DEBUG_PRINT("CPX Over UART1: ERROR - Failed to create ping timer!\n");
+  }
+#endif
+  
   isInit = true;
   DEBUG_PRINT("CPX Over UART1: Deck driver initialization complete!\n");
 }
@@ -71,6 +133,8 @@ static bool cpxOverUart1Test()
 }
 
 static const DeckDriver crtpOver1UART = {
+    .vid = 0xBC,
+    .pid = 0x0E,
     .name = "cpxOverUART1",
 
     .usedPeriph = DECK_USING_UART1,
